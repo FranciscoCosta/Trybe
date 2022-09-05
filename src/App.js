01ae8d5ffb407cea4209b8017e4fda8543526abc
Francisco Costa <francisco100eg@gmail.com>
2022-09-05 10:09:33 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import Login from './components/Login';
+import Search from './components/Search';
+import Album from './components/Album';
+import Favorites from './components/Favorites';
+import Profile from './components/Profile';
+import ProfileEdit from './components/ProfileEdit';
+import NotFound from './components/NotFound';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 
 //Francisco Costa
 
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+          <p>TrybeTunes</p>
+          <Route exact path="/" component={Login}/>
+          <Route exact path="/search" component={Search}/>
+          <Route exact path="/album/:id" component={Album}/>
+          <Route exact path="/favorites" component={Favorites}/>
+          <Route exact path="/profile" component={Profile}/>
+          <Route exact path="/profile/edit" component={ProfileEdit}/>
+          <Route path="*" component={NotFound}/>
+
+      </BrowserRouter>
+
+    );
   }
 }
 

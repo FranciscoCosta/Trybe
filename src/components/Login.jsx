33a1a3583c4f1fe0ffc1c19fb0524dc21cebd5c7
Francisco Costa <francisco100eg@gmail.com>
2022-09-05 11:30:26 -0300

    Requisito 2

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,67 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Carregando from './Carrengando';
 
+const Min = 3;
 class Login extends React.Component {
+  state = {
+    btnlogin: true,
+    carrengado: false,
+    submit: false,
+    name: '',
+  };
+
+  hadleInputChange = (event) => {
+    const { value } = event.target;
+    this.setState({ name: value });
+    if (value.length < Min) {
+      this.setState({ btnlogin: true });
+    } else {
+      this.setState({ btnlogin: false });
+    }
+  };
+
+  handleClick = () => {
+    const { name } = this.state;
+    this.setState({ submit: true,
+      carrengado: true }, async () => {
+      await createUser({ name });
+      this.setState({ carrengado: false });
+    });
+  };
+
   render() {
+    const { btnlogin, name, carrengado, submit } = this.state;
+    if (carrengado) return <Carregando />;
+    if (!carrengado && submit) return <Redirect to="/search" />;
     return (
-      <div data-testid="page-login" />
+      <div data-testid="page-login">
+        <form>
+          <label htmlFor="nome">
+            Nome:
+            <br />
+            <input
+              type="text"
+              name="name"
+              id="nome"
+              data-testid="login-name-input"
+              onChange={ this.hadleInputChange }
+              value={ name }
+            />
+          </label>
+          <br />
+          <button
+            type="button"
+            name="input-name"
+            data-testid="login-submit-button"
+            disabled={ btnlogin }
+            onClick={ this.handleClick }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
     );
   }
 }

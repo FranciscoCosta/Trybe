f8bd209bf0ca5d8c03d07232166934596e92c1a7
Francisco Costa <francisco100eg@gmail.com>
2022-09-05 13:49:46 -0300

    Requisito 5 Lint

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,42 +1,45 @@
-import { event } from 'jquery';
 import React from 'react';
 import Header from './Header';
 
-
-const Min =2;
+const Min = 2;
 class Search extends React.Component {
-  state={
+  state = {
     inputPesquisa: '',
-    submitPesquisaDis: true, 
-  }
+    submitPesquisaDis: true,
+  };
 
-  handleChange=(event)=>{
-    const {value } = event.target
-    this.setState({inputPesquisa: value})
-    {value.length>=Min ? this.setState({submitPesquisaDis: false}):
-  this.setState({submitPesquisaDis: true})}
-  }
+  handleChange = (event) => {
+    const { value } = event.target;
+    this.setState({ inputPesquisa: value });
+    if (value.length < Min) {
+      this.setState({ submitPesquisaDis: true });
+    } else {
+      this.setState({ submitPesquisaDis: false });
+    }
+  };
 
   render() {
-    const {inputPesquisa, submitPesquisaDis} = this.state
+    const { inputPesquisa, submitPesquisaDis } = this.state;
 
     return (
-      
+
       <div data-testid="page-search">
         <Header />
         <form>
-          <input 
-          type="text" 
-          name="input" 
-          data-testid="search-artist-input"
-          value={inputPesquisa} 
-          onChange={this.handleChange}
+          <input
+            type="text"
+            name="input"
+            data-testid="search-artist-input"
+            value={ inputPesquisa }
+            onChange={ this.handleChange }
           />
-          <button 
-          type="button"
-          data-testid="search-artist-button"
-          disabled={submitPesquisaDis}
-          >Pesquisar</button>
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ submitPesquisaDis }
+          >
+            Pesquisar
+          </button>
         </form>
       </div>
     );

1632425ca385ec89ab7f62869b90e923389fcc22
Francisco Costa <francisco100eg@gmail.com>
2022-09-05 12:47:22 -0300

    Requisito 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Carregando from './Carrengando';
 
@@ -28,6 +29,18 @@ class Header extends React.Component {
           {' '}
           {carregando ? <Carregando /> : utilizador.name}
         </div>
+        <nav>
+          <Link to="/search" data-testid="link-to-search">
+            Pesquisa
+          </Link>
+          <Link to="/favorites" data-testid="link-to-favorites">
+            Favoritos
+          </Link>
+          <Link to="/profile" data-testid="link-to-profile">
+            Favoritos
+          </Link>
+
+        </nav>
       </header>
     );
   }
